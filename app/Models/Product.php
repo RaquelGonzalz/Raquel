ff3eb8bc9f58c2a3df56e4d3ef84c41ca386ac74
RaquelGonzalz <rg7582241@gmail.com>
2024-09-25 21:19:53 -0600

    main

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product';
+    
+    protected $id = 'id';
+
+    public $timestamps = true;
+
+    protected $fillable =[ 
+    'name',
+    'description',
+    'price',
+    'stock'];
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+//use App\Http\Controllers
 use Illuminate\Support\Facades\Route;
 
 /*
